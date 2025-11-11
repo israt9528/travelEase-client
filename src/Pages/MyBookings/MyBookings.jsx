3d8a69ee13fe8f5db0c0323c252f49ae70253c61
israt <israt9528@gmail.com>
2025-11-11 15:46:10 +0600

    added my booking page

--- src/Pages/MyBookings/MyBookings.jsx
+++ src/Pages/MyBookings/MyBookings.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAuth from "../../hooks/useAuth";
+import Loading from "../../Components/Loading/Loading";
+import VehicleCard from "../../Components/VehicleCard/VehicleCard";
 
 const MyBookings = () => {
-  return <div>book</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [loading, setLoading] = useState(true);
+  const [vehicles, setVehicles] = useState([]);
+
+  useEffect(() => {
+    axiosSecure.get(`/my-bookings?email=${user.email}`).then((data) => {
+      console.log(data.data);
+      setVehicles(data.data);
+      setLoading(false);
+    });
+  }, [axiosSecure, user]);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  return (
+    <div>
+      book
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {vehicles.map((vehicle) => (
+          <VehicleCard key={vehicle.id} vehicle={vehicle}></VehicleCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyBookings;
